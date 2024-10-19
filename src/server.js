bf5e8940cf535d198704abd373fb1f01a3e104c0
RaviRaj <rks829421@gmail.com>
2024-10-19 11:28:57 +0530

    deploy url add

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://sparkling-sunflower-b052d2.netlify.app",
     credentials: true,
   })
 );
